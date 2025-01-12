cb64df092606f4d5ab5ed8eab090049b421be201
Bychinsky Dmitry <DBychinsky@qulix.com>
2025-01-12 18:09:15 +0300

    Добавлен параметр валюты

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const PORT = 3000;
-const apiUrl = `https://card.wb.ru/cards/v2/detail?curr=byn&dest=-59202&nm=`
+const API_URL_HTTPS = `https://card.wb.ru/cards`;
+const API_URL_VERSION = `/v2`;
+const API_URL_PARAM_ONE = `/detail`;
+const API_URL_CURRENCY = `?curr=`;
+const API_URL_PARAM_TWO = `&dest=-59202&nm=`;
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,13 +16,19 @@ app.use(function (req, res, next) {
 
 app.get(`/`, async (req, res) => {
     const id = req.query.id;
-
+    const currency = req.query.currency;
+    const apiUrl =
+        `${API_URL_HTTPS}${API_URL_VERSION}${API_URL_PARAM_ONE}${API_URL_CURRENCY}${currency}${API_URL_PARAM_TWO}${id}`
+    console.log(apiUrl)
     try {
         // Замените URL на адрес сервера, с которого вы хотите получить данные
-        const response = await axios.get(`${apiUrl}${id}`);
+        const response = await axios.get(apiUrl);
 
         // Отправляем полученные данные в формате JSON
         res.json(response.data.data.products[0]);
+
+        // setTimeout(() => res.json(response.data.data.products[0]), 1000)
+
     } catch (error) {
         console.error('Ошибка при получении данных:', error);
         res.status(500).json({error: 'Ошибка при получении данных'});
